319875da489a32dd90cc797068c714d583b35f61
Ali Ahmed <ali.ahmed@punchcard.io>
2019-10-01 23:31:29 -0600

    solved grains, rnaTranscript: C#

---
+++ csharp/Grains.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+public static class Grains
+{
+    public static ulong Square(int n)
+    {
+        if (n <= 0 || n > 64)
+        {
+            throw new ArgumentOutOfRangeException("Out of range");
+        }
+        else
+        {
+            return (ulong)Math.Pow(2, n - 1);
+        }
+    }
+
+    public static ulong Total() => 
+        Enumerable
+            .Range(1, 64)
+            .Select(x => Square(x))
+            .Aggregate((acc, x) => acc + x);
+}
---
+++ csharp/RnaTranscription.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class RnaTranscription
+{
+    public static string ToRna(string nucleotide)
+    {
+        var h = new Dictionary<Char, Char>();
+        h.Add('G', 'C');
+        h.Add('C', 'G');
+        h.Add('T', 'A');
+        h.Add('A', 'U');
+
+        return string.Concat(nucleotide.Select(x => h[x]));
+    }
+}
