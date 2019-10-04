bd8fa72adc188a4484137c6407b605a017585d42
Ali Ahmed <ali.ahmed@punchcard.io>
2019-10-03 23:39:46 -0600

    solved proverb: C#

---
+++ csharp/Proverb.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+public static class Proverb
+{
+    public static string[] Recite(string[] subjects)
+    {
+        return subjects.Select((s, i) =>
+        {
+            return i == subjects.Length - 1 ?
+                $"And all for the want of a {subjects[0]}." :
+                $"For want of a {s} the {subjects[i + 1]} was lost.";
+        }).ToArray();
+    }
+}
