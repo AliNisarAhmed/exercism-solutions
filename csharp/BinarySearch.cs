bf167b246cc15b6ed94cec76fde7e54c1730b596
Ali Ahmed <ali.ahmed@punchcard.io>
2019-10-03 23:09:52 -0600

    solved binary search and pangram

---
+++ csharp/BinarySearch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+public static class BinarySearch
+{
+    public static int Find(int[] input, int value)
+    {
+        var left = 0;
+        var right = input.Length - 1;
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            if (input[mid] == value)
+            {
+                return mid;
+            }
+            else if (value > input[mid])
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ csharp/Pangram.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input) => 
+        string
+            .Concat(
+                input
+                  .Where(s => char.IsLetter(s))
+                  .Select(s => char.ToLower(s))
+                  .Distinct()
+            ).Length == 26;
+}
