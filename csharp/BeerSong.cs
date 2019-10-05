91a0300de3e659fcf913af3690930acf7f1621a0
Ali Ahmed <ali.ahmed@punchcard.io>
2019-10-05 14:40:27 -0600

    solved Beersong, Darts, Isogram, ScrabbleScore, SumOfMultiples and Triangle: C#

---
+++ csharp/BeerSong.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public static class BeerSong
+{
+    public static string Recite(int startBottles, int takeDown)
+    {
+        var result = "";
+        while (takeDown > 0)
+        {
+            result = result + ReturnVerse(startBottles, takeDown);
+            startBottles--;
+            takeDown--;
+            if (takeDown > 0)
+                result += "\n\n";
+        }
+        return result;
+    }
+
+    public static string ReturnVerse(int first, int second)
+    {
+        if (first == 0)
+        {
+            return "No more bottles of beer on the wall, no more bottles of beer.\n" +
+            "Go to the store and buy some more, 99 bottles of beer on the wall.";
+        }
+        else
+        {
+            return $"{first} {SingularOrPlural(first)} of beer on the wall, {first} {SingularOrPlural(first)} of beer.\n" +
+            $"Take {(first == 1 ? "it" : "one")} down and pass it around, {NumberOrNoMore(first - 1)} {SingularOrPlural(first - 1)} of beer on the wall.";
+        }
+    }
+
+    public static string SingularOrPlural(int v) => v == 1 ? "bottle" : "bottles";
+
+    public static string NumberOrNoMore(int v) => v == 0 ? "no more" : v.ToString();
+
+}
---
+++ csharp/Isogram.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        var wordsOnly = word.Where(char.IsLetter);
+
+        return string.Concat(
+            wordsOnly
+              .Select(char.ToLower)
+              .Distinct()
+            ).Length == string.Concat(wordsOnly).Length;
+    }
+}
