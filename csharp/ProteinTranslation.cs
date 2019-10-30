5db73ea9f70fba01183fae749afbed91d1933828
Ali Ahmed <ali.ahmed@punchcard.io>
2019-10-29 23:28:02 -0600

    solved protein translation C#

---
+++ csharp/ProteinTranslation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ProteinTranslation
+{
+    private static readonly Dictionary<string, string> Store = new Dictionary<string, string>()
+    {
+        { "AUG", "Methionine" },
+        { "UUU", "Phenylalanine" },
+        { "UUC", "Phenylalanine" },
+        { "UUA", "Leucine" },
+        { "UUG", "Leucine" },
+        { "UCU", "Serine" },
+        { "UCC", "Serine" },
+        { "UCA", "Serine" },
+        { "UCG", "Serine" },
+        { "UAU", "Tyrosine" },
+        { "UAC", "Tyrosine" },
+        { "UGU", "Cysteine" },
+        { "UGC", "Cysteine" },
+        { "UGG", "Tryptophan" },
+        { "UAA", "STOP" },
+        { "UAG", "STOP" },
+        { "UGA", "STOP" },
+    };
+    public static IEnumerable<string> Proteins(string strand)
+    {
+        return Enumerable
+                .Range(0, strand.Length / 3)
+                .Select(i => strand.Substring(i * 3, 3))
+                .Select(code => Store[code])
+                .TakeWhile(c => c != "STOP");
+    }
+}
